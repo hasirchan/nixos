8b6edd912f1ffc5b02401708d03a1228191f4b5e
hasir <hasirchan@outlook.com>
2025-09-09 18:57:09 +0800

    Add aseprite

--- hosts/yeoz-nano/user/default.nix
+++ hosts/yeoz-nano/user/default.nix
@@ -6,6 +6,7 @@
     ../../_common-linux/user/optional
     ../../_common-linux/user/optional/graphical-env/sway
     ../../_common-linux/user/optional/pkgs/texlive.nix
+    ../../_common-linux/user/optional/pkgs/aseprite.nix
   ];
 
   home.packages = with pkgs; [
