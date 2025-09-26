d3ce487b47af9a4a22772a509958bd75bcb59ae9
hasir <hasirchan@outlook.com>
2025-09-26 20:14:57 +0800

    Add nvidia-offload script

--- hosts/yeoz-zen/system/nvidia.nix
+++ hosts/yeoz-zen/system/nvidia.nix
@@ -79,4 +79,14 @@
       intelBusId = "PCI:0@0:2:0";
     };
   };
+  environment.systemPackages = with pkgs; [
+    (writeShellScriptBin "nvidia-offload" ''
+      export __NV_PRIME_RENDER_OFFLOAD=1
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export __VK_LAYER_NV_optimus=NVIDIA_only
+      exec "$@"
+    '')
+  ];
+
 }
