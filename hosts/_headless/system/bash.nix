b31c0c64d346cde8e23c1109e7911d27002e01e0
hasir <hasirchan@outlook.com>
2025-10-26 11:39:32 +0800

    modify bash prompt

--- hosts/_headless/system/bash.nix
+++ hosts/_headless/system/bash.nix
@@ -7,7 +7,16 @@
 
       # Provide a nice prompt if the terminal supports it.
       if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
-        PS1="\n\[\033[32m\]\w\[\033[33m\]\$(GIT_PS1_SHOWUNTRACKEDFILES=1 GIT_PS1_SHOWDIRTYSTATE=1 __git_ps1)\[\033[00m\] \\$ "
+        PS1="\n\[\e[36m\]\w\[\e[m\] \$(
+          GIT_PS1_SHOWUNTRACKEDFILES=1 \
+          GIT_PS1_SHOWDIRTYSTATE=1 \
+          GIT_PS1_SHOWSTASHSTATE=1 \
+          GIT_PS1_SHOWUPSTREAM=auto \
+          GIT_PS1_SHOWCONFLICTSTATE=yes \
+          GIT_PS1_DESCRIBE_STYLE=default \
+          GIT_PS1_SHOWCOLORHINTS=1 \
+          __git_ps1 '(%s)'
+        ) \\$ "
       fi
     '';
   };
