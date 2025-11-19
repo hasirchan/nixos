b044eb326c086002f93b2261dfc7f3ab640027eb
hasir <hasirchan@outlook.com>
2025-11-19 13:23:25 +0800

    make the style and default more consistent

--- hosts/_headless/system/bash.nix
+++ hosts/_headless/system/bash.nix
@@ -4,10 +4,8 @@
     enable = true;
     blesh.enable = true;
     promptInit = ''
-
-      # Provide a nice prompt if the terminal supports it.
       if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
-        PS1="\n\[\e[36m\]\w\[\e[m\] \$(
+        GIT_PROMPT="\$(
           GIT_PS1_SHOWUNTRACKEDFILES=1 \
           GIT_PS1_SHOWDIRTYSTATE=1 \
           GIT_PS1_SHOWSTASHSTATE=1 \
@@ -16,7 +14,18 @@
           GIT_PS1_DESCRIBE_STYLE=default \
           GIT_PS1_SHOWCOLORHINTS=1 \
           __git_ps1 '(%s) '
-        )\\$ "
+        )"
+        
+        if [ -n "$INSIDE_EMACS" ]; then
+          # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
+          PS1="\n\[\e[36m\]\w\[\e[m\] $GIT_PROMPT\\$ "
+        else
+          PS1="\n\[\e]0;\u@\h: \w\a\]\[\e[36m\]\w\[\e[m\] $GIT_PROMPT\\$ "
+        fi
+        
+        if test "$TERM" = "xterm"; then
+          PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+        fi
       fi
     '';
   };
