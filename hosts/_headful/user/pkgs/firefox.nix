8b70a40f56a053d3b8015181a8d25abfab57c5e6
hasir <hasirchan@outlook.com>
2025-11-25 23:10:39 +0800

    video speed controller and violentmonkey

--- hosts/_headful/user/pkgs/firefox.nix
+++ hosts/_headful/user/pkgs/firefox.nix
@@ -7,6 +7,8 @@
       extensions.packages = with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
         ublock-origin
         asbplayer
+        violentmonkey
+        videospeed
       ];
       settings = {
         "extensions.autoDisableScopes" = 0;
