d87f12e6f2c83b332abd41f6241c3528c4b06a3b
hasir <hasirchan@outlook.com>
2026-01-07 16:44:28 +0800

    create email secrets module for home-manager

---
+++ secrets/email/default.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, self, ... }:{
+  sops.secrets = {
+    abuse_password = {
+      sopsFile = ./secrets.yaml;
+      owner = "saya";
+      mode = "0400";
+    };
+    postmaster_password = {
+      sopsFile = ./secrets.yaml;
+      owner = "saya";
+      mode = "0400";
+    };
+    dmarc-report_password = {
+      sopsFile = ./secrets.yaml;
+      owner = "saya";
+      mode = "0400";
+    };
+    mbr_password = {
+      sopsFile = ./secrets.yaml;
+      owner = "saya";
+      mode = "0400";
+    };
+  };
+  home-manager.users.saya = {
+    imports = [
+      ./client.nix
+    ];
+  };
+}
