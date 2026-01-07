db681f2f1b42f3cfcdc3fc0ac5b233d515c81afc
hasir <hasirchan@outlook.com>
2026-01-07 19:29:38 +0800

    chore(secrets): rename susops to sops

--- secrets/default.nix
+++ secrets/default.nix
@@ -7,9 +7,8 @@
 {
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   environment.systemPackages = [
-    (pkgs.writeShellScriptBin "susops" ''
+    (pkgs.writeShellScriptBin "sops" ''
       exec env SOPS_AGE_KEY_CMD="sudo ${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key" "${pkgs.sops}/bin/sops" "$@"
     '')
-    pkgs.sops
   ];
 }
