52adb50f32fb05ed64213c15338a14fea6f84dc9
hasir <hasirchan@outlook.com>
2026-01-07 18:12:21 +0800

    refactor susops script to use inline sudo for age key

--- secrets/default.nix
+++ secrets/default.nix
@@ -2,8 +2,7 @@
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   environment.systemPackages = [
     (pkgs.writeShellScriptBin "susops" ''
-      export SOPS_AGE_KEY_CMD="${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key"
-      exec sudo -E ${pkgs.sops}/bin/sops "$@"
+      exec env SOPS_AGE_KEY_CMD="sudo ${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i /etc/ssh/ssh_host_ed25519_key" "${pkgs.sops}/bin/sops" "$@"
     '')
     pkgs.sops
   ];
